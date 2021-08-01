8466cf0677c9a23f0a9dd18b9707230eb83d44d8
Paul Serre <paul27000@gmail.com>
2021-08-01 17:33:40 -0300

    Add SSL certificate

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const privateKey  = fs.readFileSync('./ssl.key', 'utf8');
+const certificate = fs.readFileSync('./ssl.cert', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
 const app = express();
 
 app.use(cors());
@@ -13,6 +20,8 @@ require("./routes/folder.routes.js")(app);
 require("./routes/wordpress.routes.js")(app);
 require("./routes/entries.routes.js")(app);
 // set port, listen for requests
-app.listen(8443, () => {
-  console.log("Server is running on port 8443.");
-});
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080);
+httpsServer.listen(8443);
